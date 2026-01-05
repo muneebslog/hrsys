a358e28196fa8b8766661ecc2492e77b821e726d
Muhammad Muneeb <itsmuneebrehman.conact@gmail.com>
2026-01-06 00:18:11 +0500

    department db done

---
+++ app/Models/Department.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Department extends Model
+{
+     protected $fillable = [
+        'name',
+        'description',
+        'head_employee_id',
+    ];
+
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
+
+    public function head()
+    {
+        return $this->belongsTo(Employee::class, 'head_employee_id');
+    }
+}
